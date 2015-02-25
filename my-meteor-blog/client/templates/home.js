26ad3909e9fede6de1fbb89f5a37e996beae673a
sorabhm <sorabhm@gmail.com>
2015-02-25 17:03:00 +0530

    Adding meteor blog code

---
+++ my-meteor-blog/client/templates/home.js
@@ -0,0 +1,20 @@
+Template.home.created= function() {
+	console.log("Home template created");
+}
+Template.home.rendered = function() {
+	console.log("Home template rendered");
+}
+Template.home.destroyed = function() {
+	console.log("Home template destroyed");
+}
+
+Template.home.helpers({
+	exampleHelpers: function() {
+		return 'This text came from a helper with some <strong>HTML</strong>.';
+	}
+})
+Template.home.helpers({
+	helperHand: function() {
+		return "Another text where <strong>HTML comes</strong>";
+	}
+})
